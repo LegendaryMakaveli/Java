507e83c1b5578d1121665e2c5e04f68f0db53720
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-12 04:05:12 +0100

    update task

--- Assignment/FunDrill/SummerFun.java
+++ Assignment/FunDrill/SummerFun.java
@@ -126,28 +126,171 @@ return newArray;
 }
 
 
+public static void positiveOrNegative() {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter an integer:  ");
+int userInput = input.nextInt();
+
+if (userInput > 0) {
+		System.out.println("Positive!");
+	} else if (userInput < 0) {
+		System.out.println("Negative!");
+	} else {
+		System.out.println("Zero!");
+	}
+
+}
+
+public static void loopingStars() {
+int counter = 0;
+for (int count = 1; count <= 5; count++) {
+	System.out.println("*");
+for (int index = 1; index <= 5; index++) {
+		counter++;
+	}
+}
+System.out.println("Total star is: " + counter);
 
 
+}
 
 
+public static void ageRestriction() {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter your age:  ");
+int userInput = input.nextInt();
+
+if (userInput >=10) {
+		System.out.println("Welcome to the show!");
+	} else {
+		System.out.println("Dey go house, you are too young!");
+	}
+
+}
+
+public static int countNumber(int number) {
+int counter = 0;
+for (int count = 1; count <= number; count++) {
+	counter+=count;
+}
+return counter;
 
 
+}
+
+public static void guessTheAnimak() {
+Scanner input = new Scanner(System.in);
+System.out.print("Guess an animal:  ");
+String userInput = input.nextLine();
+
+if (userInput.equalsIgnoreCase("dog")) {
+		System.out.println("Correct! it's a dog!");
+	} else {
+		System.out.println("Nope, it's a dog");
+	}
+
+}
+
+public static void countByTwo() {
+for (int count = 1; count <= 10; count++) {
+	 if (count % 2 == 0) {
+			System.out.print(count + ", ");
+		}
+	}
+}
 
 
+public static void temperatureCheck() {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter today's temperature:  ");
+int userInput = input.nextInt();
+
+if (userInput > 30) {
+		System.out.println("Hot!");
+	} else if (userInput < 15) {
+		System.out.println("Cold");
+	} else if (userInput >= 15 && userInput <= 30){
+		System.out.println("Nice!");
+	}
+
+}
+
+public static void printYourName() {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter your name:  ");
+String userInput = input.nextLine();
+
+for (int user = 1; user <= 3; user++) {
+		System.out.println(userInput);
+	}
+}
 
 
+public static void magicNumber() {
+Scanner input = new Scanner(System.in);
+System.out.print("Guess the magic number:  ");
+int userInput = input.nextInt();
+
+if (userInput == 42) {
+		System.out.println("You found the magic number!");
+	} else {
+		System.out.println("Keep looking!");
+	}
+}
 
 
+public static void countingUp() {
+for (int count = 1; count <= 7; count++) {
+		System.out.print(count + ", ");
+	}
+}
 
 
+public static void guessNumber() {
+Scanner input = new Scanner(System.in);
+int  userInput;
+do{
+System.out.print("Enter a number:  ");
+userInput = input.nextInt();
+
+if (userInput != 8) {
+	System.out.println("Try again");
+} else {
+	System.out.println("Correct!");
+	}
+
+}while (userInput !=  8);
+
+}
+
+public static void multiplicationTable() {
+int number = 5;
+int sum = 1;
+for (int count = 1; count <= 10; count++) {
+sum = number * count;
+}
+System.out.print(sum);
+}
 
 
+public static void gradeCalculator() {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter student score (1-100):  ");
+int userInput = input.nextInt();
 
+if (userInput >= 90) {
+		System.out.println("A");
+	} else if (userInput >= 80 && userInput < 90) {
+		System.out.println("B");
+	} else if (userInput >= 70 && userInput < 80){
+		System.out.println("C");
+	} else {
+		System.out.println("F");
+	}
 
+}
 
-
-
-
+ 
 
 
 
@@ -167,6 +310,11 @@ return newArray;
 
 
 public static void main(String... makaveli) {
+	 gradeCalculator();
+	//multiplicationTable();
+	//guessNumber();
+	//countingUp();
+	//magicNumber();
 	//greetOwner();
 	//petChooser();
 	//getFavoriteNumber();
@@ -177,5 +325,13 @@ public static void main(String... makaveli) {
 	//countToFive();
 	//countDown();
 	 //trafficLight();
+	//positiveOrNegative();
+	//loopingStars();
+	//ageRestriction();
+	//System.out.print(countByTwo(10));
+	//guessTheAnimak();
+	//countByTwo();
+	//temperatureCheck();
+	 //printYourName();
 }
 }
