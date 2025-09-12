7be013c760c2c663eee91ac4f764c1541bc284cc
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-12 09:46:45 +0100

    Air conditioning app

--- AcFolder/SourceCodeFiles/AirCondition.java
+++ AcFolder/SourceCodeFiles/AirCondition.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class AirCondition {
+    private boolean isOn = false;
+    private int temperature = 17;
+    private String name;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setTemperature(int newTemperature) {
+        if(isOn && newTemperature > 16 && newTemperature < 31 ){
+            temperature = newTemperature;
+        }
+    }
+    public void increaseTemperature(){
+        if(isOn && temperature >= 16 && temperature < 30){
+            temperature++;
+        }
+    }
+    public void decreaseTemperature(){
+        if(isOn && temperature > 16 && temperature < 30 ){
+            temperature--;
+        }
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+    public void setIsOn() {
+        isOn = true;
+    }
+    public boolean getIsOn() {
+        return isOn;
+    }
 }
