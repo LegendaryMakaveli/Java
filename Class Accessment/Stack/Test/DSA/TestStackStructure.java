0a518aeb182998122884fd233956fdfe0ece5a4d
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-25 23:47:16 +0100

    test file for stack behavior code

--- Class Accessment/Stack/Test/DSA/TestStackStructure.java
+++ Class Accessment/Stack/Test/DSA/TestStackStructure.java
@@ -1,4 +1,59 @@
 package DSA;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestStackStructure {
+    @Test
+    public void testStackStructure_isEmpty() {
+        StackStructure makaStack = new StackStructure();
+        assertTrue(makaStack.isEmpty());
+    }
+
+    @Test
+    public void  addOneElementToStackAndItsNotEmpty() {
+        StackStructure makaStack = new StackStructure();
+        makaStack.pushElement(5);
+        assertFalse(makaStack.isEmpty());
+    }
+
+    @Test
+    public void addOneElementToStackAndRemoveIt(){
+        StackStructure makaStack = new StackStructure();
+        makaStack.pushElement(5);
+        makaStack.popElement();
+        assertTrue(makaStack.isEmpty());
+    }
+
+    @Test
+    public void addTwoElementsToStackAnd(){
+        StackStructure makaStack = new StackStructure();
+        makaStack.pushElement(5);
+        makaStack.pushElement(10);
+        assertFalse(makaStack.isEmpty());
+    }
+
+    @Test
+    public void removeTwoElementsFromStackAndis_Empty(){
+        StackStructure makaStack = new StackStructure();
+        makaStack.pushElement(5);
+        makaStack.pushElement(10);
+        assertFalse(makaStack.isEmpty());
+        makaStack.popElement();
+        makaStack.popElement();
+        assertTrue(makaStack.isEmpty());
+    }
+
+    @Test
+    public void addTwoElementsRemoveOneAndIsEmptyIsFalse(){
+        StackStructure makaStack = new StackStructure();
+        makaStack.pushElement(5);
+        makaStack.pushElement(10);
+        assertFalse(makaStack.isEmpty());
+        makaStack.popElement();
+        assertFalse(makaStack.isEmpty());
+    }
+
 }
