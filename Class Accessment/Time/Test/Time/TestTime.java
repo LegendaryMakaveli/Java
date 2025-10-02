3147d635de48c77efb7bb5f8cfd640e802b03094
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-02 17:34:42 +0100

    test

--- Class Accessment/Time/Test/Time/TestTime.java
+++ Class Accessment/Time/Test/Time/TestTime.java
@@ -1,4 +1,19 @@
 package Time;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestTime {
+
+    @Test
+    public void TestThatCreateANewTimeObjectWorks() {
+        Time time = new Time(4, 44, 45);
+        time.setHour(8);
+        time.setMinute(20);
+        time.setSeconds(44);
+
+        assertTrue(time.getHour() == 8 && time.getMinute() == 20 && time.getSeconds() == 44);
+    }
 }
