d7a6ef519f28045440ee19bec2c33bf9ff379c5f
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-08 17:13:00 +0100

    Test file

--- Class Accessment/Enum/Test/GeoPoliticalZoneDriverTest.java
+++ Class Accessment/Enum/Test/GeoPoliticalZoneDriverTest.java
@@ -1,5 +1,23 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GeoPoliticalZoneDriverTest {
+    @Test
+    public void testThatStateCanBeFound(){
+        String stateToCheck = "Lagos";
+        String expectedState = GeoPoliticalZoneDriver.checkForStateInZone(stateToCheck);
+        String actual = "SOUTHWEST";
 
+        assertEquals(actual, expectedState);
+    }
+
+    @Test
+    public void testForAnUnknownState() {
+        String stateToCheck = "Ibadan";
+        String expectedState = GeoPoliticalZoneDriver.checkForStateInZone(stateToCheck);
+        String actual = "unknow state";
+
+        assertEquals(actual, expectedState);
+    }
 }
