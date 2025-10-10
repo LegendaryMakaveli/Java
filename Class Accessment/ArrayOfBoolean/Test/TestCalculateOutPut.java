82d80a4ba2e86c3b0ed8cb44f7c78dbd71b2d40e
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-10 13:44:14 +0100

    Test file for sum of two in array

--- Class Accessment/ArrayOfBoolean/Test/TestCalculateOutPut.java
+++ Class Accessment/ArrayOfBoolean/Test/TestCalculateOutPut.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestCalculateOutPut {
+    @Test
+    public void TestThatTheMethodWorksAsExpected() {
+        CalculateOutPut obj = new CalculateOutPut();
+        int [] array = {3,5,2,7,10};
+        obj.sumOfTwo(array);
+        int [] result = new int[]{3,5};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(obj.sumOfTwo(array)));
+
+    }
 }
