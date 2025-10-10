eefcad2c6a05254bc25c2e4b13735a34fd9f3497
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-10 13:43:06 +0100

    boolean palindrome

--- Class Accessment/ArrayOfBoolean/SourceCode/Palindrome.java
+++ Class Accessment/ArrayOfBoolean/SourceCode/Palindrome.java
@@ -1,2 +1,23 @@
-public class palindrome {
+public class Palindrome {
+
+    public boolean[] isPalindrome(String[] arrays) {
+        boolean[] palindrome = new boolean[arrays.length];
+
+        for (int count = 0; count < arrays.length; count++) {
+            String word = arrays[count];
+            String reversed = "";
+
+            for (int counter = word.length() - 1; counter >= 0; counter--) {
+                reversed += word.charAt(counter);
+            }
+
+            if (word.equalsIgnoreCase(reversed)) {
+                palindrome[count] = true;
+            } else {
+                palindrome[count] = false;
+            }
+        }
+
+        return palindrome;
+    }
 }
