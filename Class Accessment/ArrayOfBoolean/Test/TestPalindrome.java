4bfccb46dd63312f98e951fdf1bc94451239d2c5
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-10 13:44:47 +0100

    test file for parlindrome

--- Class Accessment/ArrayOfBoolean/Test/TestPalindrome.java
+++ Class Accessment/ArrayOfBoolean/Test/TestPalindrome.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestPalindrome {
+
+    @Test
+    public void TestIsPalindrome() {
+        Palindrome checkPalindrome = new Palindrome();
+        String[] words = {"racecar", "hello", "cohort", "madam"};
+
+        boolean[] result = checkPalindrome.isPalindrome(words);
+
+        boolean[] expected = {true, false, false, true};
+
+        assertArrayEquals(expected, result);
+    }
 }
