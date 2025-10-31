20d404fcb0c25d739a7a63d4ca30d4f41e121a83
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-10-31 16:36:21 +0100

    Creatiing file for phoneBookn management

---
+++ BankAccountFolder/SourceCodes/Account.java
@@ -0,0 +1,12 @@
+public class Bank {
+    private double balance;
+    private String fullName;
+    private int numberOfCustomers;
+
+    public Bank(String fullName) {
+        this.fullName = fullName;
+    }
+
+
+
+}
